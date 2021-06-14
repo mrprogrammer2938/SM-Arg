2aff12249a475e86eee57606a52ea6f132cc6342
Mr.programmer <78996423+mrprogrammer2938@users.noreply.github.com>
2021-06-14 01:53:59 -0400

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+# This code write by Mr.nope
+clear
+echo "Installing..."
+sleep 2
+chmod +x smarg
+sudo cp smarg /usr/local/bin
+echo ""
+echo "Installing..., Finish...!"
+echo ""
+exit 1
---
+++ uninstall.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+# This code write by Mr.nope
+clear
+echo "Uninstalling..."
+sleep 2
+cd /usr/local/bin && sudo rm smarg
+echo ""
+echo "Uninstalling..., Finish...!"
+echo ""
+exit 1
